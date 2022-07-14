f9528d62f97d66a59c8655328b8738747ad3c7e7
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-07-13 22:04:11 -0400

    Implemented the Math.ABS method

--- P3_NumberGuessing/P3_NumberGuessing/Program.cs
+++ P3_NumberGuessing/P3_NumberGuessing/Program.cs
@@ -8,44 +8,47 @@ namespace P3_NumberGuessing // Note: actual namespace depends on the project nam
         static void Main(string[] args)
         {
             Random draw = new Random();
-            int drawedNumber = draw.Next(1, 101);
+            int drawedNumber = draw.Next(1, 21);
             int guessedNumber = 0;
+            
 
             Console.WriteLine("Let's play! \nYou will have 5 chances to win this game. \nGuess a number between 1 to 100. \n");
 
             //For to iterantes through 5 times
             for (int count = 1; count <= 6; count++)
-            {   
+            {
                 //Validates whether or not the player reached their limited tries 
                 if (count == 6)
                 {
                     Console.WriteLine($"You lose -.- \nThe correct number was {drawedNumber}.");
                 }
-                
+
                 //The player continues to play if they haven't surpassed 5 times
                 else
                 {
                     Console.Write($"Guess number {count}: ");
                     guessedNumber = Int32.Parse(Console.ReadLine());
+                    int distance = Math.Abs(drawedNumber - guessedNumber);
 
-                        
-                        if (guessedNumber == drawedNumber)
-                        {
-                            Console.WriteLine($"Congratulations!!! You guessed the right number {drawedNumber}.");
-                            break;
-                        }
-                        if (guessedNumber >= (drawedNumber - 5) && guessedNumber <= (drawedNumber + 5))
-                        {
-                            Console.WriteLine("You are close! ");
-                        }
-                        if (guessedNumber < drawedNumber)
-                        {
-                            Console.WriteLine("Your guess was too low.");
-                        }
-                        if (guessedNumber > drawedNumber)
-                        {
-                            Console.WriteLine("Your guess was too high.");
-                        }
+
+                    if (guessedNumber == drawedNumber)
+                    {
+                        Console.WriteLine($"Congratulations!!! You guessed the right number {drawedNumber}.");
+                        break;
+                    }
+                    //if (guessedNumber >= (drawedNumber - 5) && guessedNumber <= (drawedNumber + 5))
+                    if (distance <= 5)
+                    {
+                        Console.WriteLine("You are close! ");
+                    }
+                    if (guessedNumber < drawedNumber)
+                    {
+                        Console.WriteLine("Your guess was too low.");
+                    }
+                    if (guessedNumber > drawedNumber)
+                    {
+                        Console.WriteLine("Your guess was too high.");
+                    }
                        
                 }
                     
