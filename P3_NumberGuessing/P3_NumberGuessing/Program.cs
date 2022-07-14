14d04f56c412772aa262b7172651ee688a4a0ba1
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-07-13 21:00:33 -0400

    Removed GetRandomNumber function

--- P3_NumberGuessing/P3_NumberGuessing/Program.cs
+++ P3_NumberGuessing/P3_NumberGuessing/Program.cs
@@ -5,19 +5,10 @@ namespace P3_NumberGuessing // Note: actual namespace depends on the project nam
     internal class Program
     {
 
-       //Function to get a Random number for the main program
-        static int GetARandomNumber()
+        static void Main(string[] args)
         {
             Random draw = new Random();
             int drawedNumber = draw.Next(1, 101);
-            return drawedNumber;
-        }
-
-
-        //Main program
-        static void Main(string[] args)
-        {
-            int randomNumber = GetARandomNumber();
             int guessedNumber = 0;
 
             Console.WriteLine("Let's play! \nYou will have 5 chances to win this game. \nGuess a number between 1 to 100. \n");
